bf1663eefc146c570eb70e29938c203e902ffdff
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-10-22 20:34:11 +0300

    add: slices

---
+++ src/redux/store.js
@@ -0,0 +1,39 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const contactsConfig = {
+  key: 'contactsList',
+  storage,
+  whitelist: ['contacts'],
+  //   blacklist: ['filter'],
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+export const store = configureStore({
+  reducer: persistReducer(contactsConfig, rootReducer),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
